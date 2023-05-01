71e8539a0cc07f5a3a764a1fe351c58b4fb2047c
kafil <18-38175-2@student.aiub.edu>
2023-05-02 04:44:16 +0600

    rollback+main in _document.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
     <Header title="Home" />
     
 
-    <body
+    <main
       className=" mx-auto px-4 bg-slate-200"
     >
       <h2 className="text-2xl font-bold mb-4 text-center">New Releases</h2>
@@ -16,7 +16,7 @@ const Home = () => {
 
       <h2 className="text-2xl font-bold my-4 text-center">Popular</h2>
       <CarouselComponent />
-    </body>
+    </main>
     </>
   );
 };
