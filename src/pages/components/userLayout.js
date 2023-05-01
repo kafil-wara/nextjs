ee48ad1f748ba1de4a12db523202f2d37c2e6986
kafil <18-38175-2@student.aiub.edu>
2023-05-02 01:47:49 +0600

    userlayout updated to handle null

--- src/pages/components/userLayout.js
+++ src/pages/components/userLayout.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 import Link from 'next/link';
 
 export default function UserLayout({ user }) {
-  const [isBlocked, setIsBlocked] = useState(String(user.isblocked));
+  const [isBlocked, setIsBlocked] = useState(user?.isblocked || false);
 
   const handleBlockUser = () => {
-    const status = isBlocked === "true" ? "false" : "true";
+    const status = isBlocked ? false : true;
     // Make API request to block/unblock the user
     axios
-      .put('https://nestjs-production-8281.up.railway.app/admin/blockuser', { id: user.id, status })
+      .put('https://nestjs-production-8281.up.railway.app/admin/blockuser', { id: user?.id, status })
       .then(response => {
         // Handle successful response
         console.log('User blocked/unblocked successfully:', response.data);
         setIsBlocked(status); // Update the block status in the state
-        alert(`User has been ${status === "true" ? "blocked" : "unblocked"} successfully!`);
+        alert(`User has been ${status ? 'blocked' : 'unblocked'} successfully!`);
       })
       .catch(error => {
         // Handle error
@@ -23,6 +23,10 @@ export default function UserLayout({ user }) {
       });
   };
 
+  if (!user) {
+    // Handle the case when the user object is undefined or null
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="bg-gray-100 min-h-screen flex justify-center items-center">
@@ -46,19 +50,18 @@ export default function UserLayout({ user }) {
             </tr>
             <tr>
               <td className="font-bold py-2">Block Status:</td>
-              <td className="py-2">{isBlocked}</td>
+              <td className="py-2">{isBlocked.toString()}</td>
             </tr>
           </tbody>
         </table>
         <button
-          className={`mt-4 py-2 px-4 rounded border-2 border-gray-300 ${isBlocked === "true" ? "bg-red-500 text-white-500" : "bg-green-500 text-white-500"
+          className={`mt-4 py-2 px-4 rounded border-2 border-gray-300 ${isBlocked ? 'bg-red-500 text-white-500' : 'bg-green-500 text-white-500'
             }`}
           onClick={handleBlockUser}
         >
-          {isBlocked === "true" ? "Unblock User" : "Block User"}
+          {isBlocked ? 'Unblock User' : 'Block User'}
         </button>
       </div>
     </div>
   );
-
 }
