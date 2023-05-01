55066d338fcd2a631592ca716ab77a05f28996f8
kafil <18-38175-2@student.aiub.edu>
2023-05-02 03:47:05 +0600

    12

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,11 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-      </Head>
+    <Html>
+      <Head />
+      <body>
         <Main />
         <NextScript />
+      </body>
     </Html>
   )
 }
